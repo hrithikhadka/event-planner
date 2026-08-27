249f5c10ff20b0d31f63aaa92f5dffb2365dee08
hrithikhadka <hrithikhadka00@gmail.com>
2026-08-27 23:11:54 +0300

    navbar and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Home Page
-    </div>
-  );
+  return <div>Home Page</div>;
 }
